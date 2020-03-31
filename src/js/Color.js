c5d2be657e8d3be5e05feeaaba1b70fa233880cf
Dariusz Okonski <darek200180@gmail.com>
2020-03-31 13:45:07 +0100

    add class Color with methods changeColor and getColor

---
+++ src/js/Color.js
@@ -0,0 +1,17 @@
+class Color {
+  constructor(setColor) {
+    this.color = setColor;
+  }
+
+  changeColor(setColor) {
+    this.color = setColor;
+  }
+
+  getColor() {
+    return this.color;
+  }
+}
+
+
+
+export default Color;
