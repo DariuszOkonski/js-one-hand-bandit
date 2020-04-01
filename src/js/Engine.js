9897cab634334078b8b915bcdde4e8832bde8480
Dariusz Okonski <darek200180@gmail.com>
2020-04-01 13:24:00 +0100

    add this.wallet, this.bid, method renderWallet, getMoney and make changes at rollEngine method

--- src/js/Engine.js
+++ src/js/Engine.js
@@ -1,17 +1,26 @@
 import Colors from './Colors';
+import Wallet from './Wallet';
+
+const START_AMOUNT = 200;
 
 class Engine {
   constructor() {
     this.colors = new Colors();
+    this.wallet = new Wallet(START_AMOUNT)
+    this.bid = 0;
+
     this.cardColors = document.querySelectorAll('.card-deck .card');
     this.btnRoll = document.getElementById('btn-roll').addEventListener('click', (e) => this.rollEngine(e));
     this.btnReset = document.getElementById('btn-reset').addEventListener('click', (e) => this.reset(e));
+    this.moneyInput = document.getElementById('money-input');
+
 
     this.render();
   }
 
   render() {
     this.renderCardColors();
+    this.renderWallet();
   }
 
   renderCardColors() {
@@ -20,17 +29,46 @@ class Engine {
     })
   }
 
+  renderWallet() {
+    document.getElementById('wallet').textContent = this.wallet.getAmount();
+  }
   ///======================================
   rollEngine(e) {
-    this.setRandomColors();
+    this.bid = this.getMoney(e);
+    console.log(this.bid)
+    if (this.bid === undefined)
+      return;
+
+    this.wallet.removeFromAmount(this.bid);
+
 
     this.render();
   }
 
   reset(e) {
     this.colors = new Colors();
+    this.wallet = new Wallet(START_AMOUNT);
+    this.bid = 0;
 
     this.render();
+  }
+
+  getMoney(e) {
+    let amount = Number(this.moneyInput.value);
+
+    if (amount < 0 || amount > this.wallet.getAmount()) {
+      amount = 0;
+    }
+
+    if (amount === 0) {
+      alert('You have to bet first');
+      this.moneyInput.value = 0;
+      return
+    }
+
+    this.setRandomColors();
+    this.moneyInput.value = 0;
+    return Math.floor(amount);
   }
 
   setRandomColors() {
